ccec953f230e3f165f3fac2c376de82ec93f34b5
Nadav Lewit <nadavl@Nadavs-Air.lan>
2022-07-04 12:22:11 +0300

    Finished implementing the reversal calculator and its methods

---
+++ Model/ReversalCalculator.js
@@ -0,0 +1,51 @@
+import {Board} from "./Board.js"
+
+class ReversalCalculator{
+    constructor(board, boardAsList, numRows){
+        this.board = board
+        this.boardAsList = boardAsList
+        this.numRows = numRows
+    }
+
+    getNumReversals(){
+        let totalReversals = 0
+        for (let index = 0; index < this.boardAsList.length - 1; index++) {
+            for (let compared = index + 1; compared < this.boardAsList.length; compared++) {
+                if(this.boardAsList[index].identifier > this.boardAsList[compared].identifier){
+                    totalReversals++
+                }
+            }
+        }
+
+        return totalReversals
+    }
+
+    getBlankSquareRow(){
+        let rowIndex = 0
+        let rowCounter = 0
+        this.board.forEach(row => {
+            row.forEach(square => {
+                if(square.identifier == 0){
+                    rowIndex = rowCounter + 1
+                }
+            });
+            rowCounter++
+        });
+
+        return rowIndex
+    }
+
+    isSolvable(){
+        let score = this.getNumReversals()
+
+        if(this.numRows % 2 == 0){
+            score += this.getBlankSquareRow()
+        }
+        return (score % 2 == 0)
+    }
+}
+
+let board  = new Board(3)
+let gameBoard = board.gameBoard()
+let calculator = new ReversalCalculator(gameBoard, board.boardAsList, board.numRows)
+console.log(calculator.isSolvable())
