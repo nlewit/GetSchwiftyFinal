9441316706985c4378963f6e61a1d79b9f18eddb
Nadav Lewit <nadavl@Nadavs-Air.lan>
2022-07-04 19:30:34 +0300

    Created a basic button and made sure it works

--- View/BoardDisplayer.js
+++ View/BoardDisplayer.js
@@ -1,3 +1,16 @@
 export class BoardDisplayer{
-    
+
+    createButton(button_display){
+        let btn = document.createElement("button")
+        btn.innerHTML = button_display
+        btn.type = "button"
+        btn.onclick = function () {
+            alert("Button is clicked");
+        };
+
+        document.body.appendChild(btn);
+    }
 }
+
+let displayer = new BoardDisplayer()
+displayer.createButton("hi")
